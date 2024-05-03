bf5587d98bbe4659d63f41b0c85f25eb9b3d0348
THEOD0R1 <squirrelnub@gmail.com>
2024-05-03 10:07:41 +0200

    added ob start in account register

--- AccountRegister.php
+++ AccountRegister.php
@@ -1,10 +1,8 @@
-
-
 <?php
-require_once('lib/PageTemplate.php');
-require_once( 'vendor/autoload.php');
-require_once('Models/Database.php');
-require_once('Utils/Validator.php');
+require_once ('lib/PageTemplate.php');
+require_once ('vendor/autoload.php');
+require_once ('Models/Database.php');
+require_once ('Utils/Validator.php');
 # trick to execute 1st time, but not 2nd so you don't have an inf loop
 if (!isset($TPL)) {
     $TPL = new PageTemplate();
@@ -13,13 +11,12 @@ if (!isset($TPL)) {
     include "layout.php";
     exit;
 }
-
-
+ob_start();
 
 
 $dbContext = new DbContext();
 $message = "";
-$username ="";
+$username = "";
 $registeredOk = false;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -67,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $message = "Registration attempt cancelled.";
         } catch (\Delight\Auth\AuthError $e) {
             $message = "Authentication error: " . $e->getMessage();
-        }catch (\Exception $e) {
+        } catch (\Exception $e) {
             $message = "An error occurred during registration: " . $e->getMessage();
         }
     } else {
@@ -78,41 +75,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 <p>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="newsletter">
-                <p><strong>User Registration</strong></p>
-                <form method="post" action="AccountRegister.php">
-                    <input class="input" type="email" placeholder="Enter Your Email" name="username" value="<?php echo isset($_POST['username']) ? $_POST['username'] : ''; ?>">
-                    <br/><br/>
-                    <input class="input" type="password" placeholder="Enter Your Password" name="password">
-                    <br/><br/>
-                    <input class="input" type="password" placeholder="Repeat Password" name="password-confirmation">
-                    <br/><br/>
-                    <input class="input" type="text" placeholder="Name" name="givenname" value="<?php echo isset($_POST['givenname']) ? $_POST['givenname'] : ''; ?>">
-                    <br/><br/>
-                    <input class="input" type="text" placeholder="Street Address" name="street" value="<?php echo isset($_POST['street']) ? $_POST['street'] : ''; ?>">
-                    <br/><br/>
-                    <input class="input" type="text" placeholder="Postal Code" name="zip" value="<?php echo isset($_POST['zip']) ? $_POST['zip'] : ''; ?>">
-                    <br/><br/>
-                    <input class="input" type="text" placeholder="City" name="city" value="<?php echo isset($_POST['city']) ? $_POST['city'] : ''; ?>">
-                    <br/><br/>
-                    <button class="newsletter-btn" type="submit"><i class="fa fa-envelope"></i> Register</button>
-                </form>
-                <?php
-                if (isset($validationErrors)) {
-                    echo '<p>' . $messages . '</p>';
-                    echo '<ul>';
-                    foreach ($validationErrors as $error) {
-                        echo '<li>' . $error . '</li>';
-                    }
-                    echo '</ul>';
+<div class="row">
+    <div class="col-md-12">
+        <div class="newsletter">
+            <p><strong>User Registration</strong></p>
+            <form method="post" action="AccountRegister.php">
+                <input class="input" type="email" placeholder="Enter Your Email" name="username"
+                    value="<?php echo isset($_POST['username']) ? $_POST['username'] : ''; ?>">
+                <br /><br />
+                <input class="input" type="password" placeholder="Enter Your Password" name="password">
+                <br /><br />
+                <input class="input" type="password" placeholder="Repeat Password" name="password-confirmation">
+                <br /><br />
+                <input class="input" type="text" placeholder="Name" name="givenname"
+                    value="<?php echo isset($_POST['givenname']) ? $_POST['givenname'] : ''; ?>">
+                <br /><br />
+                <input class="input" type="text" placeholder="Street Address" name="street"
+                    value="<?php echo isset($_POST['street']) ? $_POST['street'] : ''; ?>">
+                <br /><br />
+                <input class="input" type="text" placeholder="Postal Code" name="zip"
+                    value="<?php echo isset($_POST['zip']) ? $_POST['zip'] : ''; ?>">
+                <br /><br />
+                <input class="input" type="text" placeholder="City" name="city"
+                    value="<?php echo isset($_POST['city']) ? $_POST['city'] : ''; ?>">
+                <br /><br />
+                <button class="newsletter-btn" type="submit"><i class="fa fa-envelope"></i> Register</button>
+            </form>
+            <?php
+            if (isset($validationErrors)) {
+                echo '<p>' . $messages . '</p>';
+                echo '<ul>';
+                foreach ($validationErrors as $error) {
+                    echo '<li>' . $error . '</li>';
                 }
-                else {
-                    echo '<p>' . $message . '</p>';
-                }
-                ?>
-            </div>
+                echo '</ul>';
+            } else {
+                echo '<p>' . $message . '</p>';
+            }
+            ?>
         </div>
     </div>
+</div>
 </p>
