1a88990d0d751e702db13f0fe0f67f672e7ca6ec
Fika03 <filipkarlstrom03@gmail.com>
2024-05-03 10:32:20 +0200

    table creation

--- Models/Database.php
+++ Models/Database.php
@@ -1,37 +1,40 @@
 <?php
-require_once('Models/Customer.php');
-require_once('Models/UserDatabase.php');
+require_once ('Models/Customer.php');
+require_once ('Models/UserDatabase.php');
 
-class DBContext{
+class DBContext
+{
 
 
     private $pdo;
     private $usersDatabase;
     private $initialized = false;
 
-    function getUsersDatabase(){
+    function getUsersDatabase()
+    {
         return $this->usersDatabase;
     }
 
-    function __construct() {  
+    function __construct()
+    {
 
         $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '../../');
         $dotenv->load();
 
 
         $host = $_ENV['host'];
-        $db   = $_ENV['db'];
+        $db = $_ENV['db'];
         $user = $_ENV['user'];
         $pass = $_ENV['pass'];
         $dsn = "mysql:host=$host;dbname=$db";
         $this->pdo = new PDO($dsn, $user, $pass);
-        $this->usersDatabase = new UserDatabase($this->pdo); 
+        $this->usersDatabase = new UserDatabase($this->pdo);
         $this->initIfNotInitialized();
-        
+
     }
 
-  function addUser($givenname, $street, $city, $zip, $user_id)
-{
+    function addUser($givenname, $street, $city, $zip, $user_id)
+    {
 
         $prep = $this->pdo->prepare("INSERT INTO UserDetails (givenname, street, city, zip, user_id)
                                      VALUES (:givenname, :street, :city, :zip, :user_id)");
@@ -44,28 +47,52 @@ class DBContext{
         ]);
         return $this->pdo->lastInsertId();
 
-}
-
-function initIfNotInitialized() {
-    if ($this->initialized) {
-        return;
     }
 
-    $sql = "CREATE TABLE IF NOT EXISTS `UserDetails` (
-        `id` int NOT NULL AUTO_INCREMENT,
-        `givenname` varchar(50) NOT NULL,
-        `street` varchar(50) NOT NULL,
-        `city` varchar(50) NOT NULL,
-        `zip` varchar(10) NOT NULL,
-        `user_id` int(10) unsigned NOT NULL,
-        PRIMARY KEY (`id`),
-        FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-        )ENGINE=MyISAM";
+    function initIfNotInitialized()
+    {
+        if ($this->initialized) {
+            return;
+        }
 
-    $this->pdo->exec($sql);
-    $this->usersDatabase->setupUsers();
-    $this->initialized = true;
-  }
+        $sql1 = "CREATE TABLE IF NOT EXISTS `UserDetails` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `givenname` varchar(50) NOT NULL,
+            `street` varchar(50) NOT NULL,
+            `city` varchar(50) NOT NULL,
+            `zip` varchar(10) NOT NULL,
+            `user_id` int(10) unsigned NOT NULL,
+            PRIMARY KEY (`id`),
+            FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+            ) ENGINE=MyISAM";
+
+        $this->pdo->exec($sql1);
+
+        $sql2 = "CREATE TABLE IF NOT EXISTS `QueueDetails` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `name` varchar(50) NOT NULL,
+            `creationdate` datetime NOT NULL,
+            PRIMARY KEY (`id`)
+        )";
+
+        $this->pdo->exec($sql2);
+
+        $sql3 = "CREATE TABLE IF NOT EXISTS `Queue` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `date` datetime NOT NULL,
+            `active` boolean NOT NULL,
+            `queue_id` int NOT NULL,
+            
+            PRIMARY KEY (`id`),
+            FOREIGN KEY (`queue_id`) REFERENCES `QueueDetails` (`id`)
+        )";
+
+        $this->pdo->exec($sql3);
+
+
+        $this->usersDatabase->setupUsers();
+        $this->initialized = true;
+    }
 }
 
 
