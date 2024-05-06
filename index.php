90a24286b7e7d32998e203ea021bc95360ec3044
Fika03 <filipkarlstrom03@gmail.com>
2024-05-06 10:51:55 +0200

    link depentent on logged in or not

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-require_once('lib/PageTemplate.php');
-require_once('vendor/autoload.php');
-require_once('Models/Database.php');
+require_once ('lib/PageTemplate.php');
+require_once ('vendor/autoload.php');
+require_once ('Models/Database.php');
 
 
 # trick to execute 1st time, but not 2nd so you don't have an inf loop
@@ -15,42 +15,45 @@ if (!isset($TPL)) {
 ?>
 <img src="img/halfcircle.svg" alt="circle" class="bg-img">
 <img src="img/halfcircletop.svg" alt="circle" class="bg-img-top">
-    <div class="row-1">
-        <div class="info-container">
-            <h1>Your easy to use <br> digital helplist</h1>
-            <p>Streamlined administration and communication in one place. <br> <b>EduQ</b> - the comprehensive solution for school efficiency and effectiveness.</p>
-                <div class="button-container">
-                    <a href="/BookDemo.php"><button class="filled-btn">Book Demo</button></a>
-                    <a href="/Solutions.php"><button class="hollow-btn">Our Solutions</button></a>
-                </div>
-
-                <div class="costumer-container">
-                    <p><i class="fa-solid fa-circle-question"></i><b> Already a customer?</b> <a href="/CreateQueueRoom.php">Get started!</a></p>
-                   
-                </div>
+<div class="row-1">
+    <div class="info-container">
+        <h1>Your easy to use <br> digital helplist</h1>
+        <p>Streamlined administration and communication in one place. <br> <b>EduQ</b> - the comprehensive solution for
+            school efficiency and effectiveness.</p>
+        <div class="button-container">
+            <a href="/BookDemo.php"><button class="filled-btn">Book Demo</button></a>
+            <a href="/Solutions.php"><button class="hollow-btn">Our Solutions</button></a>
         </div>
 
-        <div class="img-container"><img src="img/screen_v2.png" alt="example" class="example-img"></div>
+        <div class="costumer-container">
+            <p><i class="fa-solid fa-circle-question"></i><b> Already a customer?</b> <a
+                    href="<?php echo ($dbContext->getUsersDatabase()->getAuth()->isLoggedIn()) ? '/CreateQueueRoom.php' : '/AccountLogin.php'; ?>">Get
+                    started!</a></p>
+
+        </div>
     </div>
 
-    <div class="row-2">
+    <div class="img-container"><img src="img/screen_v2.png" alt="example" class="example-img"></div>
+</div>
+
+<div class="row-2">
     <div class="img-container">
         <img src="img/helpimage.jpg" alt="Help Image" class="help-img">
     </div>
     <div class="functionality-container">
         <h2 id="messenger"></h2>
-        <p>EduQ is meticulously designed to streamline school operations across all demographics, fostering inclusivity and efficiency. It provides a secure foundation for seamless management.</p>
+        <p>EduQ is meticulously designed to streamline school operations across all demographics, fostering inclusivity
+            and efficiency. It provides a secure foundation for seamless management.</p>
         <p><b>Highlighted Features:</b></p>
-            <li><i class="fa-solid fa-circle-check"></i> Attendance Tracking</li>
-            <li><i class="fa-solid fa-circle-check"></i> Communication Hub</li>
-            <li><i class="fa-solid fa-circle-check"></i> Assignment Submission</li>
-            <li><i class="fa-solid fa-circle-check"></i> Resource Sharing</li>
-            <li><i class="fa-solid fa-circle-check"></i> Customizable Dashboards</li>
-            <li><i class="fa-solid fa-circle-check"></i> Queue Management</li>
-            <li><i class="fa-solid fa-circle-check"></i> Assessment Tools</li>
+        <li><i class="fa-solid fa-circle-check"></i> Attendance Tracking</li>
+        <li><i class="fa-solid fa-circle-check"></i> Communication Hub</li>
+        <li><i class="fa-solid fa-circle-check"></i> Assignment Submission</li>
+        <li><i class="fa-solid fa-circle-check"></i> Resource Sharing</li>
+        <li><i class="fa-solid fa-circle-check"></i> Customizable Dashboards</li>
+        <li><i class="fa-solid fa-circle-check"></i> Queue Management</li>
+        <li><i class="fa-solid fa-circle-check"></i> Assessment Tools</li>
     </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="textScroller.js"></script>  
-
+<script src="textScroller.js"></script>
